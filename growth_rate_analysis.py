1a4d42b32ed68d5b1d756da4cfd46ad6ca07085b
David Klein <kleinjdavid@gmail.com>
2017-11-10 10:34:31 -0500

    move files around

--- growth_rate_analysis.py
+++ growth_rate_analysis.py
@@ -4,14 +4,14 @@ import pandas
 import sys
 import matplotlib
 
-location = '~/Documents/Shmoo_Lab/Flexostat-interface/test_data_'
+location = '~/Documents/Shmoo_Lab/Flexostat-interface/Data/10-20-17/test_data_'
 od_df = pandas.read_csv('{}od.csv'.format(location), header = None, names = ["Time", "1", "2", "3", "4", "5", "6", "7", "8"])
 u_df = pandas.read_csv('{}u.csv'.format(location), header = None, names = ["Time", "1", "2", "3", "4", "5", "6", "7", "8"])
 z_df = pandas.read_csv('{}z.csv'.format(location), header = None, names = ["Time", "1", "2", "3", "4", "5", "6", "7", "8"])
 
 for chamber in range(1,9):
 	od_df.plot.scatter(x='Time', y='{}'.format(chamber))
-	plt.savefig('ch{}_scatter.png'.format(chamber))
+	plt.savefig('Data/10-20-17/od/ch{}_scatter.png'.format(chamber))
 
 test_data_r = []
 start = u_df.ix[0,0]
@@ -28,10 +28,10 @@ for row in u_df.itertuples():
 			new_row.append(round((numpy.log(element)/new_row[0]),6))
 	test_data_r.append(new_row)
 
-numpy.savetxt("test_data_r.csv", test_data_r, delimiter=",")
+numpy.savetxt("Data/10-20-17/test_data_r.csv", test_data_r, delimiter=",")
 
 r_df = pandas.read_csv('{}r.csv'.format(location), header = None, names = ["Time", "1", "2", "3", "4", "5", "6", "7", "8"])
 
 for chamber in range(1,9):
 	r_df.plot.scatter(x='Time', y='{}'.format(chamber))
-	plt.savefig('ch{}_growth_rate.png'.format(chamber))
+	plt.savefig('Data/10-20-17/growth/ch{}_growth_rate.png'.format(chamber))
