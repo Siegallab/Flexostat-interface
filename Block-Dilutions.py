210754cae499506cbc3539a67a9a2c53ce1736d0
David Klein <kleinjdavid@gmail.com>
2018-04-18 16:07:03 -0400

    fixed incorrect blocking

--- Block-Dilutions.py
+++ Block-Dilutions.py
@@ -1,4 +1,3 @@
-
 """
 BACKGROUND
 
@@ -16,7 +15,9 @@ INSTRUCTIONS
 
 import os
 import csv
+import json
 import time
+import numpy
 import argparse
 from math import log10
 from datetime import datetime
@@ -50,10 +51,10 @@ def main():
 
 		# If blocklog doesn't exist, start dilution blocks and create
 		if not os.path.exists(log['blocklog']) and args.schedule:
-			programlog = programlog[0:2] + [','.join(controller['setpoint'].split()), human_time, machine_time, ','.join(str(e) for e in current_ods)]
+			programlog = programlog[0:3] + [','.join(controller['setpoint'].split()), human_time, machine_time, ','.join(str(e) for e in current_ods)]
 			update_log(args, log, programlog)
 		elif not os.path.exists(log['blocklog']) and args.chamber:
-			programlog = programlog[0:2] + [','.join(controller['setpoint'].split()), human_time, machine_time, ','.join(str(e) for e in current_ods)]
+			programlog = programlog[0:3] + [','.join(controller['setpoint'].split()), human_time, machine_time, ','.join(str(e) for e in current_ods)]
 			update_log(args, log, programlog)
 		else:
 			blocklog_file = open(log['blocklog'], 'r')
@@ -62,17 +63,17 @@ def main():
 
 			# Update controller and programlog if block interval elapsed
 			if args.schedule:
-				controller, programlog = check_blockinterval(current_ods, log, controller, programlog, prevlog)
+				controller, programlog = check_blockinterval(current_ods, controller, programlog, prevlog)
 			# Update controller and programlog with new setpoints and chamber report when each chamber reaches their setpoint
 			else:
-				controller, programlog = compare_ods(args, current_ods, controller, programlog)
+				controller, programlog = compare_ods(current_ods, controller, programlog)
 			# Update config and log if setpoints have been changed
 			if not prevlog[3] == programlog[3]:
 				update_config(args, config, controller)
 				update_log(args, log, programlog)
 	else:
 		print('ERROR: Config file not found or function not specified correctly.')
-	print('Program end.\n')
+	print('Block-Dilutions.py end.')
 
 
 def command_line_arguments():
@@ -94,8 +95,8 @@ def command_line_arguments():
 	parser.add_argument('--delay', default='0', help="delay startup of first run by minutes")
 	parser.add_argument('--config', default='config.ini', help="change config file from default 'config.ini'")
 	parser.add_argument('-o', '--out', action='store_true', help='program processes printing')
-	parser.add_argument('-b', '--block', default='0', help='specify hour interval for block (default config, otherwise 5)')
-	parser.add_argument('-d', '--dilution', default='0', help='specify hour interval for dilution (default config, otherwise 2.5)')
+	parser.add_argument('-g', '--growth', default='0', help='specify hour interval for growth block (default config, otherwise 7)')
+	parser.add_argument('-d', '--dilution', default='0', help='specify hour interval for dilution (default config, otherwise 4)')
 	parser.add_argument('-c', '--chamber', action='store_true', help='use individual chamber OD for dilutions')
 	parser.add_argument('-s', '--schedule', action='store_true', help='use interval dilution schedule for dilutions')
 
@@ -114,19 +115,17 @@ def read_ods(args, log):
 	# If full log argument specified, then use flexoparse code to get ODs from fullog file
 	if args.full_log:
 		log_file = open(log['fulllog'], 'r')
-		first = log_file.readlines()[0]
-		time_start = int(first.split(":")[1][:-7])
-		line = log_file.readlines()[-1]
+		log_data = log_file.readlines()
 		log_file.close()
-
-		time_start = int(first.split(":")[1][:-7])
-		current_ods = []
-		d1 = line.split(":")
-		machine_time = int(d1[1][:-7])
+		first = log_data[0]
+		time_start = json.loads(first)['timestamp']
+		last_line = log_data[-1]
+		if len(last_line) < 1:
+			last_line = log_data[-2]
+		last_line = json.loads(last_line)
+		current_ods = list(last_line['ods'])
+		machine_time = last_line['timestamp']
 		human_time = (machine_time - time_start) / 3600
-		ods = d1[2][2:-6].split(",")
-		for od in ods:
-			current_ods.append(float(od))
 	# Otherwise od log will be read, then compute ODs from blank and odlog file
 	else:
 		blank_file = open(log['blanklog'], 'r')
@@ -174,15 +173,15 @@ def update_config(args, config, controller):
 			time.sleep(float(args.delay)*60)
 	# If block interval, set to config otherwise 1 if not specified, save as float, and update config to match
 	if args.schedule:
-		if float(args.block) <= 0 and len(controller['blockinterval']) == 0:
-			args.block = 5
-		elif float(args.block) <= 0:
-			args.block = float(controller['blockinterval'])
+		if float(args.growth) <= 0 and len(controller['growthinterval']) == 0:
+			args.growth = 7
+		elif float(args.growth) <= 0:
+			args.growth = float(controller['growthinterval'])
 		if float(args.dilution) <= 0 and len(controller['dilutioninterval']) == 0:
-			args.dilution = 5
+			args.dilution = 4
 		elif float(args.dilution) <= 0:
 			args.dilution = float(controller['dilutioninterval'])
-		controller['blockinterval'] = args.block
+		controller['growthinterval'] = args.growth
 		controller['dilutioninterval'] = args.dilution
 	config['controller'] = controller
 	config_update = open(args.config, 'w')
@@ -190,13 +189,12 @@ def update_config(args, config, controller):
 	return controller
 
 
-def check_blockinterval(current_ods, log, controller, programlog, prevlog):
+def check_blockinterval(current_ods, controller, programlog, prevlog):
 	"""
 	Determines if the block interval time has passed.
 	Updates controller and programlog appropriately.
 
 	:param current_ods: real ods from od log or full log file
-	:param log: config file log variables
 	:param controller: config file controller variables
 	:param programlog: list of updated status
 	:param prevlog: list of previous status
@@ -206,7 +204,7 @@ def check_blockinterval(current_ods, log, controller, programlog, prevlog):
 	diff = datetime.now() - past
 	# If block interval reached (elapsed time = diff between current time and last blocklog entry)
 	#	then update the controller setpoints appropriately
-	if controller['setpoint'] == controller['savesetpoint'] and (diff.seconds/3600) >= float(controller['blockinterval']):
+	if controller['setpoint'] == controller['savesetpoint'] and (diff.seconds/3600) >= float(controller['growthinterval']):
 		controller['setpoint'] = controller['blockstart']
 	elif controller['setpoint'] == controller['blockstart'] and (diff.seconds/3600) >= float(controller['dilutioninterval']):
 		controller['setpoint'] = controller['savesetpoint']
@@ -215,12 +213,11 @@ def check_blockinterval(current_ods, log, controller, programlog, prevlog):
 	return controller, programlog
 
 
-def compare_ods(args, current_ods, controller, programlog):
+def compare_ods(current_ods, controller, programlog):
 	"""
 	Compares the current ods with the set points.
 	Updates controller and programlog appropriately.
 
-	:param args: command line arguments for program
 	:param current_ods: real ods from od log or full log file
 	:param controller: controller parameters from config file
 	:param programlog: list of updated status
@@ -231,11 +228,11 @@ def compare_ods(args, current_ods, controller, programlog):
 	save_ods = list(map(float, controller['savesetpoint'].split()))
 	for num in range(8):
 		# If setpoint has original values, check if ODs have reached up to within 5% of setpoint
-		if reference_ods[num] == save_ods[num] and not current_ods[num] >= (reference_ods[num]-(reference_ods[num]*0.05)):
+		if reference_ods[num] == save_ods[num] and current_ods[num] >= (reference_ods[num]-(reference_ods[num]*0.05)):
 			# Update setpoint OD with new setpoint
 			reference_ods[num] = block_ods[num]
 		# Otherwise setpoint has blockstart values, check if ODs have reached down to within 5% of setpoint
-		elif not current_ods[num] <= (reference_ods[num]+(reference_ods[num]*0.05)):
+		elif current_ods[num] <= (reference_ods[num]+(reference_ods[num]*0.05)):
 			# Update setpoint OD with new setpoint
 			reference_ods[num] = save_ods[num]
 	controller['setpoint'] = ' '.join(str(e) for e in reference_ods)
@@ -253,7 +250,7 @@ def update_log(args, log, programlog):
 	:param programlog: list of updated status
 	"""
 	if args.out:
-		print('\n'.join(programlog))
+		print(programlog)
 	blocklog_file = open(log['blocklog'], 'a')
 	wr = csv.writer(blocklog_file)
 	wr.writerow(programlog)
